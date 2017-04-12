cc9cbedf63bc90505ccb5053e395235af3af9fd0
Mark Hebert <mark@hasissues.com>
2017-04-11 22:15:24 -0400

    fix build for ionic 3

--- android-release.bat
+++ android-release.bat
@@ -3,10 +3,13 @@
 call ionic cordova:build android --release
 IF %ERRORLEVEL% == 0 (
     @echo -- signing --
-    call "%JAVA_HOME%\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-release-unsigned.apk alias_name
+    call "%JAVA_HOME%\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-armv7-release-unsigned.apk alias_name
+    call "%JAVA_HOME%\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms\android\build\outputs\apk\android-x86-release-unsigned.apk alias_name
 )
 IF %ERRORLEVEL% == 0 (
     @echo -- zipalign --
-    call  del platforms\android\build\outputs\apk\android-release-signed.apk
-    call "%ANDROID_HOME%\build-tools\24.0.3\zipalign.exe" -v 4 platforms\android\build\outputs\apk\android-release-unsigned.apk platforms\android\build\outputs\apk\android-release-signed.apk
+    call del platforms\android\build\outputs\apk\android-armv7-release-signed.apk
+    call del platforms\android\build\outputs\apk\android-x86-release-signed.apk
+    call "%ANDROID_HOME%\build-tools\24.0.3\zipalign.exe" -v 4 platforms\android\build\outputs\apk\android-armv7-release-unsigned.apk platforms\android\build\outputs\apk\android-armv7-release-signed.apk
+    call "%ANDROID_HOME%\build-tools\24.0.3\zipalign.exe" -v 4 platforms\android\build\outputs\apk\android-x86-release-unsigned.apk platforms\android\build\outputs\apk\android-x86-release-signed.apk
 )
